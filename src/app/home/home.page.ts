ffc89bd0c922168ea0fd4cf17d9e3ed139d916ea
Antonny Santos <antonnysantos@MacBook-Pro-de-Antonny.local>
2024-09-12 16:37:42 -0300

    feature: add braze content cards simple implementations

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,3 +1,4 @@
+import { NgForOf, NgIf } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import {
   IonHeader,
@@ -12,12 +13,31 @@ declare var BrazePlugin: any;
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, NgForOf, NgIf],
 })
 export class HomePage implements OnInit {
+  title = 'Hi, Minder';
+  cards: any[] = [];
+
   constructor() {}
 
+  private async fetchContentCards() {
+    await BrazePlugin.requestContentCardsRefresh();
+    await BrazePlugin.getContentCardsFromCache(
+      (data: any) => {
+        this.cards = data.map(({ extras, ...item }: any) => ({ ...item, extras: JSON.parse(extras)}));
+        console.log(this.cards);
+        console.info(
+          `Succesfully fetched content cards with: ${this.cards.length} card`
+        );
+      },
+      (err: any) => console.error('Error fetching cards')
+    );
+  }
+
   ngOnInit(): void {
-    BrazePlugin.changeUser('user-id-ios');
+    BrazePlugin.changeUser('user-02');
+
+    this.fetchContentCards();
   }
 }
