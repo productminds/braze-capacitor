9d0ce5d8e2a6e91281eea6bde7b619c2400bdb10
Antonny Santos <antonnysantos@MacBook-Pro-de-Antonny.local>
2025-02-27 18:56:20 -0300

    test: inapp subscription function

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,11 +1,13 @@
 import { NgForOf, NgIf } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { Platform } from '@ionic/angular';
 import {
   IonHeader,
   IonToolbar,
   IonTitle,
   IonContent,
 } from '@ionic/angular/standalone';
+
 declare var BrazePlugin: any;
 
 @Component({
@@ -19,25 +21,46 @@ export class HomePage implements OnInit {
   title = 'Hi, Minder';
   cards: any[] = [];
 
-  constructor() {}
+  constructor(private platform: Platform) {}
 
-  private async fetchContentCards() {
-    await BrazePlugin.requestContentCardsRefresh();
-    await BrazePlugin.getContentCardsFromCache(
-      (data: any) => {
-        this.cards = data.map(({ extras, ...item }: any) => ({ ...item, extras: JSON.parse(extras)}));
-        console.log(this.cards);
-        console.info(
-          `Succesfully fetched content cards with: ${this.cards.length} card`
-        );
-      },
-      (err: any) => console.error('Error fetching cards')
+  ngOnInit() {
+    this.platform.ready().then(() => {
+      setTimeout(() => {
+        this.initializeBraze();
+      }, 1000); // Slight delay (500ms) before initiali zing Braze
+    });
+  }
+
+  initializeBraze() {
+    // Ensure deviceready is fired and web view is initialized
+    document.addEventListener(
+      'deviceready',
+      this.onDeviceReady.bind(this),
+      false
     );
   }
 
-  ngOnInit(): void {
+  onDeviceReady() {
+    this.subscribeToInAppMessages();
+    this.addInAppMessageListener();
     BrazePlugin.changeUser('user-02');
+  }
 
-    this.fetchContentCards();
+  subscribeToInAppMessages() {
+    if (BrazePlugin) {
+      BrazePlugin.subscribeToInAppMessage(true);
+    }
+  }
+
+  addInAppMessageListener() {
+    // Ensure Braze listener is added after deviceready
+    window.addEventListener('inAppMessageReceived', (event: any) => {
+      console.log('Received In-App Message:', event.detail);
+      this.handleInAppMessage(event.detail);
+    });
+  }
+
+  handleInAppMessage(message: any) {
+    console.log('Handling In-App Message:', message);
   }
 }
